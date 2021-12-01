e7a79536d2686a048bbf3e0304ddaeac79bc06ec
kdevy <ykinoshita.cal0@gmail.com>
2021-12-02 02:12:01 +0900

    add php cs fixer config.

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->exclude([
+                'vendor'
+            ])
+            ->in(__DIR__)
+    );
